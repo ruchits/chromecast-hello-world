4ff250f3ec69f919c139b7ef251b443ce07dcb7a
Ruchit Sharma <ruchitsharma@Ruchit-Sharmas-MacBook-Pro.local>
2013-11-29 14:49:46 -0500

    chromecast: fixing some bugs with the demo code

--- helloWorld.js
+++ helloWorld.js
@@ -16,8 +16,14 @@ var applicationID = '3d817983-0b16-499c-89f2-531a3fbf8b95';
 var castApi;
 var currentActivityId = null;
 var currentReceiver;
-var currentVolume = 0.5;
 var receivers = [];
+var media = [
+           'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4',
+           'http://commondatastorage.googleapis.com/gtv-videos-bucket/ED_1280.mp4',
+           'http://commondatastorage.googleapis.com/gtv-videos-bucket/tears_of_steel_1080p.mov',
+           'http://commondatastorage.googleapis.com/gtv-videos-bucket/reel_2012_1280x720.mp4',
+           'http://commondatastorage.googleapis.com/gtv-videos-bucket/Google%20IO%202011%2045%20Min%20Walk%20Out.mp3'];
+var currentMedia = media[0];
 
 if (window.cast && window.cast.isAvailable) {
   // Already initialized
@@ -37,37 +43,40 @@ initializeCastApi = function() {
 };
 
 function onReceiverList(list) {
-  console.log("receiver list" + list);
-  var receiverDiv = document.getElementById('receivers');
-  var temp = ''; 
+  if( list.length > 0 ) {
+    console.log("receiver list" + list);
+    var receiverDiv = document.getElementById('receivers');
+    var temp = ''; 
 
-  for( var i=0; i < list.length; i++ ) {
-    receivers.push(list[i]);
-    temp += '<div style="float:left; padding:10px; margin:10px;">' + list[i].name;
-    temp += '<img src="icons/drawable-mdpi/ic_media_route_off_holo_light.png" id="cast';
-    temp += list[i].id + '" onclick="castMedia(' + i + ')"></div>';
+    for( var i=0; i < list.length; i++ ) {
+      receivers.push(list[i]);
+      temp += '<div style="float:left; padding:10px; margin:10px;">' + list[i].name;
+      temp += '<img src="icons/drawable-mdpi/ic_media_route_off_holo_light.png" id="cast';
+      temp += list[i].id + '" onclick="castMedia(' + i + ')"></div>';
+    }
+    console.log(temp);
+    receiverDiv.innerHTML = temp;
+    document.getElementById("receiver_msg").innerHTML = "Click a Chromecast icon to cast video";
   }
-  console.log(temp);
-  receiverDiv.innerHTML = temp;
+  else {
+    console.log("receiver list empty");
+    document.getElementById("receiver_msg").innerHTML = "No Chromecast devices found";
+  }
 }
 
 function selectMedia(m) {
   console.log("media selected" + m);
-  document.getElementById('video').src = m;
+  currentMedia = media[m]; 
 }
 
 function castMedia(i) {
-  if( currentActivityId ) return;
-
   console.log("casting media to" + receivers[i]);
   currentReceiver = receivers[i];
 
   var launchRequest = new cast.LaunchRequest(applicationID, receivers[i]);
   launchRequest.parameters = '';
 
-  var videoSrc = document.getElementById('video');
-
-  var loadRequest = new cast.MediaLoadRequest(videoSrc.currentSrc);
+  var loadRequest = new cast.MediaLoadRequest(currentMedia);
   loadRequest.autoplay = true;
 
   castApi.launch(launchRequest, function(status) {
@@ -75,7 +84,7 @@ function castMedia(i) {
       currentActivityId = status.activityId;
       castApi.loadMedia(currentActivityId,
                               loadRequest,
-                              updateUI);
+                              launchCallback);
     } else {
       console.log('Launch failed: ' + status.errorString);
     }
@@ -83,75 +92,10 @@ function castMedia(i) {
 
 }
 
-function updateUI(status) {
+function launchCallback(status) {
   if( status.success == true ) {
-    var media_control = document.getElementById('media_control');
-    media_control.style.display = 'block';
-
     var icon_id = currentReceiver.id;
     var cast_icon = document.getElementById('cast' + icon_id);
-    cast_icon.src = 'icons/drawable-mdpi/ic_media_route_on_holo_light.png'; 
+    cast_icon.src = 'icons/drawable-mdpi/ic_media_route_on_holo_light.png';
   }
-}
-
-function stopActivity() {
-  castApi.stopActivity(currentActivityId, function() {
-    var cast_icon = document.getElementById("cast" + currentReceiver.id);
-    cast_icon.src = "icons/drawable-mdpi/ic_media_route_off_holo_light.png";
-    currentActivityId = null;
-  });
-}
-
-function getActivityStatus() {
-  castApi.getActivityStatus(currentActivityId, function(status) {
-    var activity_status = document.getElementById('activity_status');
-    activity_status.innerHTML = status.status;
-  });
-}
-
-function getMediaStatus() {
-  castApi.getMediaStatus(currentActivityId, function(status) {
-    var media_status = document.getElementById('media_status');
-    media_status.innerHTML = JSON.stringify(status.status);
-  });
-}
-
-function playMedia() {
-  castApi.playMedia(
-      currentActivityId,
-      new cast.MediaPlayRequest(),
-      function() {});
-}
-
-function pauseMedia() {
-  castApi.pauseMedia(
-      currentActivityId,
-      function() {});
-}
-
-function muteMedia() {
-  castApi.setMediaVolume(
-      currentActivityId,
-      new cast.MediaVolumeRequest(0, true),
-      function() {});
-}
-
-function unmuteMedia() {
-  castApi.setMediaVolume(
-      currentActivityId,
-      new cast.MediaVolumeRequest(currentVolume, false),
-      function() {});
-}
-
-function setVolume(v) {
-  castApi.setMediaVolume(
-      currentActivityId,
-      new cast.MediaVolumeRequest(v, false),
-      function() {});
-}
-
-function changeVolume() {
-  console.log("change volume");
-  currentVolume = document.getElementById('volume').value/10;
-  setVolume(currentVolume);
 }
